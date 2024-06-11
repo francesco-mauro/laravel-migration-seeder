cecebe032f82a49495f4be405f40eea6634009ec
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-11 16:28:34 +0200

    add query SQL and table

--- database/migrations/2024_06_11_135153_create_trains_table.php
+++ database/migrations/2024_06_11_135153_create_trains_table.php
@@ -8,11 +8,31 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * 
+     * CREATE TABLE `trains` (
+     * `id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+     * `stazione_partenza` VARCHAR(255) NOT NULL,
+     * `stazione_arrivo` VARCHAR(255) NOT NULL,
+     * `orario_partenza` TIME NOT NULL,
+     * `orario_arrivo` TIME NOT NULL,
+     * `codice_treno` VARCHAR(255) NOT NULL,
+     * `in_orario` TINYINT(1) NOT NULL DEFAULT 1,
+     * `created_at` TIMESTAMP NULL,
+     * `updated_at` TIMESTAMP NULL,
+     * PRIMARY KEY(`id`)
+     * );
+     * 
      */
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno');
+            $table->boolean('in_orario')->default(true);
             $table->timestamps();
         });
     }
