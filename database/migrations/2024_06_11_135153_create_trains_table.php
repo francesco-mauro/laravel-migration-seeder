416a5333624d01dbb5d2064917f5f84158ac0eaf
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-11 16:47:38 +0200

    feat: rollback of table and table adjustments

--- database/migrations/2024_06_11_135153_create_trains_table.php
+++ database/migrations/2024_06_11_135153_create_trains_table.php
@@ -11,29 +11,35 @@ return new class extends Migration
      * 
      * CREATE TABLE `trains` (
      * `id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+     * `azienda` VARCHAR(255) NOT NULL,
      * `stazione_partenza` VARCHAR(255) NOT NULL,
      * `stazione_arrivo` VARCHAR(255) NOT NULL,
      * `orario_partenza` TIME NOT NULL,
      * `orario_arrivo` TIME NOT NULL,
      * `codice_treno` VARCHAR(255) NOT NULL,
+     * `numero_carrozze` INT NOT NULL,
      * `in_orario` TINYINT(1) NOT NULL DEFAULT 1,
+     * `cancellato` TINYINT(1) NOT NULL DEFAULT 0,
      * `created_at` TIMESTAMP NULL,
      * `updated_at` TIMESTAMP NULL,
      * PRIMARY KEY(`id`)
      * );
      * 
+
      */
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
             $table->string('stazione_partenza');
             $table->string('stazione_arrivo');
             $table->time('orario_partenza');
             $table->time('orario_arrivo');
             $table->string('codice_treno');
+            $table->integer('numero_carrozze');
             $table->boolean('in_orario')->default(true);
-            $table->timestamps();
+            $table->boolean('cancellato')->default(false);
         });
     }
 
