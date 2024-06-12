f2449e1d04914bedd0d7717bb35bfbc00b3cc511
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-12 17:31:39 +0200

    new seed with faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+
+use Faker\Generator as Faker;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,22 +13,19 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $newTrain = new Train();
-        $newTrain->azienda = "Trenitalia";
-        $newTrain->stazione_partenza = "Zagarolo";
-        $newTrain->stazione_arrivo = "Roma";
-        $newTrain->orario_partenza = "13:00:00";
-        $newTrain->orario_arrivo = "14:00:00";
-        $newTrain->codice_treno = 44232466;
-        $newTrain->numero_carrozze = 22;
-        $newTrain->in_orario = true;
-        $newTrain->cancellato = false;
+        $newTrain->azienda = $faker->company;
+        $newTrain->stazione_partenza = $faker->city;
+        $newTrain->stazione_arrivo = $faker->city;
+        $newTrain->orario_partenza = $faker->time;
+        $newTrain->orario_arrivo = $faker->time;
+        $newTrain->codice_treno = $faker->unique()->numerify('TR#####');
+        $newTrain->numero_carrozze = $faker->numberBetween(5, 15);
+        $newTrain->in_orario = $faker->boolean;
+        $newTrain->cancellato = $faker->boolean;
         $newTrain->save();       //se non ho fatto errori, ora salva, prego Rende che funzioni
 
-        dd($newTrain); 
     }
 }
-
-
